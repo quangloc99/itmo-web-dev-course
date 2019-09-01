fbd446f72fb42bbb5d3cdc9565a206759ce86ccc
Tran Quang Loc <quangloctran99@gmail.com>
2019-09-01 11:56:37 +0300

    [lab1] Using session to store queries.

--- lab1/respond.php
+++ lab1/respond.php
@@ -1,10 +1,27 @@
 <?php
+//phpinfo();
 require_once 'server/Query.php';
 
-$que = Query::fromAssociativeArray($_GET);
+session_start();
+
+if (!array_key_exists('queries', $_SESSION)) {
+    $_SESSION['queries'] = array();
+}
+$queries = &$_SESSION['queries'];
+$queries[] = Query::fromAssociativeArray($_GET);
+
 ?>
+<head>
+    <meta charset="UTF-8">
+</head>
 <body>
     <table>
-        <?= $que->toHTMLTableRow()?>
+        <tbody>
+        <?php
+            foreach ($queries as $query) {
+                echo ($query->toHTMLTableRow());
+            }
+        ?>
+        </tbody>
     </table>
 </body>
